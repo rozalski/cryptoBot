3abcc41e90bfc1b1c83b8e83debf1d9516639340
rozalski <anfx@mail.ru>
2018-03-13 21:19:01 +0300

    delete unnecessary functionality

--- classes/CriptoBotWorker.php
+++ classes/CriptoBotWorker.php
@@ -62,11 +62,9 @@ class CriptoBotWorker {
             'from_bot' => isset($inputParams['message']['forward_from']['is_bot'])?
             $inputParams['message']['forward_from']['is_bot']:NULL
         ];
-       
-       // 
+
         $this->censoringMessage();
-       // $this->removeKeyboard();
-//         $this->keyboardinit();
+
     }
     private function removeKeyboard(){
        $removeKeyboard = json_encode([
@@ -74,9 +72,7 @@ class CriptoBotWorker {
         ]);      
         $reply_markup = '&reply_markup='.$removeKeyboard;
         $this->writeLogDelMessage(BotToken . "/sendmessage?chat_id=" .
-            $this->workinf['chatId'] . "&text=1".$reply_markup); 
-//         file_get_contents(BotToken . "/sendmessage?chat_id=" .
-//            $this->workinf['chatId'] . "&text=1&reply_markup=replykeyboardremove");       
+            $this->workinf['chatId'] . "&text=1".$reply_markup);      
     }
 
     private function keyboardinit() {
@@ -86,8 +82,7 @@ class CriptoBotWorker {
     $reply = json_encode($resp);
     file_get_contents(BotToken . "/sendmessage?chat_id=" .
             $this->workinf['chatId'] . "&text=". urlencode('test')."&reply_markup=" . $reply);  
-//    $this->writeLogDelMessage(BotToken . "/sendmessage?chat_id=" .
-//            $this->workinf['chatId'] . "&text=". urlencode('test')."&reply_markup=" . $reply. "\n");
+
     return;
 }
     public function sendMessage($message) {
@@ -205,56 +200,6 @@ class CriptoBotWorker {
         fclose($file);
 
         return;
-    }
-
-    public function sendMainKeyboard() {
-
-        $inline_button1 = array("text" => "BTC/USD", "callback_data" => "/btc-usd");
-        $inline_button2 = array("text" => "ETH/USD", "callback_data" => '/eth-usd');
-        $inline_button3 = array("text" => "ETC/USD", "callback_data" => '/etc-usd');
-        $inline_button4 = array("text" => "XRP/USD", "callback_data" => '/xrp-usd');
-        $inline_button5 = array("text" => "BCH/USD", "callback_data" => '/bch-usd');
-        $inline_button6 = array("text" => "ADA/USD", "callback_data" => '/ada-usd');
-        $inline_button7 = array("text" => "LTC/USD", "callback_data" => '/ltc-usd');
-        $inline_button8 = array("text" => "XEM/USD", "callback_data" => '/xem-usd');
-        $inline_button9 = array("text" => "NEO/USD", "callback_data" => '/neo-usd');
-        $inline_button10 = array("text" => "XLM/USD", "callback_data" => '/xlm-usd');
-        $inline_keyboard = [array($inline_button1, $inline_button2),
-            array($inline_button3, $inline_button4), array($inline_button5,
-                $inline_button6), array($inline_button7, $inline_button8),
-            array($inline_button9, $inline_button10)];
-        $keyboard = array("inline_keyboard" => $inline_keyboard);
-        $replyMarkup = json_encode($keyboard);
-        $message = "Выбери валютную пару";
-        file_get_contents(BotToken . "/sendmessage?chat_id=" . $this->workinf['chatId'] .
-                "&text=" . urlencode($message) . '&reply_markup=' . $replyMarkup);
-    }
-
-    public function sendStartKeyboard() {
-        $inline_button1 = array("text" => "\xF0\x9F\x9A\x80 Основной чат", "url" => "https://t.me/traders_crypto");
-        $inline_button2 = array("text" => "\xE2\x98\x95 Флудлка", "url" => 'https://t.me/fortraders_flood');
-
-        $inline_keyboard = [[$inline_button1], [$inline_button2]];
-        $keyboard = array("inline_keyboard" => $inline_keyboard);
-        $replyMarkup = json_encode($keyboard);
-        $message = HelpText;
-
-        file_get_contents(BotToken . "/sendmessage?chat_id=" . $this->workinf['chatId'] .
-                "&parse_mode=HTML&text=" . urlencode($message) . '&reply_markup=' . $replyMarkup . "&resize_keyboard=true");
-    }
-
-    public function getCurs($val) {
-        $content = file_get_contents("https://api.cryptonator.com/api/full/" . $val);
-        $res = json_decode($content, true);
-        foreach ($res['ticker']['markets'] as $market) {
-            $marketprices .= "На <b>" . $market['market'] . "</b> торгуется по: " .
-                    substr($market['price'], 0, -6) . "\r\n";
-        }
-        $curs = "<b>Валютная пара:</b> " . $res['ticker']['base'] . " / " . $res['ticker']['target'] .
-                "\r\n<b>Цена:</b> " . substr($res['ticker']['price'], 0, -6) .
-                "\n<b>Изменение:</b> " . substr($res['ticker']['change'], 0, -6) .
-                "\n" . $marketprices;
-        $this->sendCallBackMessage($curs);
     }
 
     private function getTime() {
